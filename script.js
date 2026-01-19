c294302eb19df68d893509c424f25fa1edcdc273
alhilalfathi <alhilal.fathi@gmail.com>
2026-01-19 17:45:03 +0700

    feat: add script using jquery

--- script.js
+++ script.js
@@ -1,52 +1,49 @@
 
-
-const inputCelcius = document.getElementById("input")
-const form = document.getElementById("inputParent")
-const tbody = document.getElementById("tbody")
-
-
-let dataKonversi = JSON.parse(localStorage.getItem("konversiSuhu")) || []
-let no = dataKonversi.length + 1
+$(document).ready(()=>{
+    const inputCelcius = $("#input")
+    const form = $("#inputParent")
+    const tbody = $("#tbody")
 
 
-dataKonversi.forEach((item, index) => {
-    renderTbody(item, index + 1)
-})
-
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-
-    const c = parseFloat(inputCelcius.value)
-    if (isNaN(c)) return
-
-    const data = {
-        celcius: c,
-        fahrenheit: (c * 9 / 5) + 32,
-        reamur: c * 4 / 5,
-        kelvin: c + 273.15
-    }
-
-   
-    dataKonversi.push(data)
-    localStorage.setItem("konversiSuhu", JSON.stringify(dataKonversi))
-
-    renderTbody(data, no++)
-})
+    let dataKonversi = JSON.parse(localStorage.getItem("konversiSuhu")) || []
+    let no = dataKonversi.length + 1
 
 
-function renderTbody(data, nomor){
-        const tr = document.createElement("tr")
-        const td1 = document.createElement("td")
-        td1.textContent = `${nomor}`
-        const td2 = document.createElement("td")
-        td2.textContent = `${data.celcius.toFixed(2)}`
-        const td3 = document.createElement("td")
-        td3.textContent = `${data.fahrenheit.toFixed(2)}`
-        const td4 = document.createElement("td")
-        td4.textContent = `${data.reamur.toFixed(2)}`
-        const td5 = document.createElement("td")
-        td5.textContent = `${data.kelvin.toFixed(2)}`
+    dataKonversi.forEach((item, index) => {
+        renderTbody(item, index + 1)
+    })
+
+    form.on("submit", function (e) {
+        e.preventDefault()
+
+        const c = parseFloat(inputCelcius.val())
+        if (isNaN(c)) return
+
+        const data = {
+            celcius: c,
+            fahrenheit: (c * 9 / 5) + 32,
+            reamur: c * 4 / 5,
+            kelvin: c + 273.15
+        }
+
+        dataKonversi.push(data)
+        localStorage.setItem("konversiSuhu", JSON.stringify(dataKonversi))
+
+        renderTbody(data, no++)
+
+    })
+
+    function renderTbody(data, nomor){
+        const tr = $("<tr></tr>")
+
+        const td1 = $(`<td>${nomor}</td>`)
+        const td2 = $(`<td>${data.celcius.toFixed(2)}</td>`)
+        const td3 = $(`<td>${data.fahrenheit.toFixed(2)}</td>`)
+        const td4 = $(`<td>${data.reamur.toFixed(2)}</td>`)
+        const td5 = $(`<td>${data.kelvin.toFixed(2)}</td>`)
         tr.append(td1,td2,td3,td4,td5)
-
         tbody.append(tr)
+        
     }
+})
+
